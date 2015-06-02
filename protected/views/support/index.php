634450ac430fc19377f0c36dc97f52e2e141a4bf
Цигельников Владислав Сергеевич <tsigelnikovvlad@gmail.com>
2015-06-02 06:33:55 +0600

    no message

--- protected/views/support/index.php
+++ protected/views/support/index.php
@@ -1,13 +1,13 @@
 	<div class="support clearfix">
 	<?php $form=$this->beginWidget('CActiveForm'); ?>
 		<div>
-			<h6>Создать обращение в службу поддержки</h6>
+			<h6 class="sup">Создать обращение в службу поддержки</h6>
 			<?php echo $form->textArea($new,'question',array('required'=>true,'placeholder' => 'Кратко опишите суть вашей проблемы, мы постараемся ответить как можно быстрее.')); ?>
 			<?php echo $form->error($new,'question'); ?>
 		</div>
 		<input type="submit" class="green-button right" value="Отправить">
 	<?php $this->endWidget(); ?>	
-		<h6 class="often">Часто задаваемые вопросы</h6>
+		<h6 class="sup often">Часто задаваемые вопросы</h6>
 		<div id="accordion">
 			<? foreach ($faq as $item): ?>
 				<h3><?=$this->replaceToBr($item->question)?></h3>
